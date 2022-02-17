d22ec6abdc69f6198f04d3ae9449bdc342633a14
arpit patel <arpatel@sigmasolve.net>
2022-02-17 16:34:27 +0530

    first commit

---
+++ themes/whill-eu-2021/partial/page-content-readytoride.php
@@ -0,0 +1,25 @@
+<div class="bl-bg readytoride">
+	<div class="c-container">
+		<h2 class="text-center">Pronto a passare a WHILL?</h2>
+		<div class="c-grid">
+			<?php $hide = true; if($hide == false): ?>
+			<div class="c-grid__u_1of2 c-grid__u_medium_3of12 c-grid__u_small_6of12 text-center">
+				<a href="/gb/schedule-a-test-drive"><img src="<?php echo get_template_directory_uri();?>/assets/dist/images/icon-testdrive.png" alt="Personal EV">
+				<p>Schedule a Test Drive</p></a>
+			</div>
+			<?php endif; ?>
+			<div class="c-grid__u_1of2 c-grid__u_medium_4of12 c-grid__u_small_6of12 text-center">
+				<a href="/it/where-to-buy"><img src="<?php echo get_template_directory_uri();?>/assets/dist/images/icon-shopping-cart.png" alt="">
+				<p>Dove acquistare</p></a>
+			</div>
+			<div class="c-grid__u_1of2 c-grid__u_medium_4of12 c-grid__u_small_6of12 text-center">
+				<a href="https://faq.whill.inc/eu/s/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/assets/dist/images/icon-question-mark.png" alt="">
+				<p>Domande frequenti</p></a>
+			</div>
+			<div class="c-grid__u_1of2 c-grid__u_medium_4of12 c-grid__u_small_6of12 text-center">
+				<a href="/it/contact" class="icon-bottom-contact"><img src="<?php echo get_template_directory_uri();?>/assets/dist/images/contact_us_email.png" alt="">
+				<p>Contattaci</p></a>
+			</div>
+		</div>
+	</div>
+</div>
